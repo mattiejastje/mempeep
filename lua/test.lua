99bdeaefab8ac600d54581c211e9e0697a57c147
mattiejas <mattiejastje@gmail.com>
2026-04-01 20:35:27 +0100

    Add ctype lua module for dumping types and struct declarations in C style.

--- lua/test.lua
+++ lua/test.lua
@@ -4,6 +4,7 @@ for _, name in ipairs({
   "test_descriptors",
   "test_read",
   "test_log_tracer",
+  "test_ctype",
 }) do
   local ok, err = pcall(require, name)
   if ok then
---
+++ lua/test/test_ctype.lua
@@ -0,0 +1,97 @@
+local d = require("mempeep.descriptors")
+local c = require("mempeep.ctype")
+
+local Int8 = d.Primitive("i1")
+local Int16 = d.Primitive("i2")
+local Int32 = d.Primitive("i4")
+local Int64 = d.Primitive("i8")
+local Point = d.Struct("Point", { d.Field(Int64, "x"), d.Field(Int32, "y") })
+local Node = d.Struct("Node", { d.Field(Int64, "data"), d.Field(d.RawAddr(), "next") })
+
+do
+  local size, typ = c.desc_to_ctype(Int8, 4)
+  assert(size == 1)
+  assert(typ == "int8_t")
+end
+
+do
+  local size, typ = c.desc_to_ctype(Int64, 4)
+  assert(size == 8)
+  assert(typ == "int64_t")
+end
+
+do
+  local size, typ = c.desc_to_ctype(Point, 4)
+  assert(size == 12)
+  assert(typ == "Point")
+end
+
+do
+  local size, typ = c.desc_to_ctype(d.RawAddr(), 4)
+  assert(size == 4)
+  assert(typ == "void*")
+end
+
+do
+  local size, typ = c.desc_to_ctype(d.RawAddr(), 8)
+  assert(size == 8)
+  assert(typ == "void*")
+end
+
+do
+  local size, typ = c.desc_to_ctype(d.Ref(Point), 4)
+  assert(size == 4)
+  assert(typ == "Point*")
+end
+
+do
+  local size, typ = c.desc_to_ctype(d.Ref(Point), 8)
+  assert(size == 8)
+  assert(typ == "Point*")
+end
+
+do
+  local size, typ = c.desc_to_ctype(d.NullableRef(Point), 4)
+  assert(size == 4)
+  assert(typ == "Point*")
+end
+
+do
+  local size, typ = c.desc_to_ctype(d.NullableRef(Point), 8)
+  assert(size == 8)
+  assert(typ == "Point*")
+end
+
+do
+  local size, typ = c.desc_to_ctype(d.Array(Point, 10), 4)
+  assert(size == 120)
+  assert(typ == "std::array<Point, 10>")
+end
+
+do
+  local size, typ = c.desc_to_ctype(d.Vector(Point, 0x1000), 4)
+  assert(size == 8)
+  assert(typ == "std::vector<Point>")
+end
+
+do
+  local size, typ = c.desc_to_ctype(d.Vector(Point, 0x1000), 8)
+  assert(size == 16)
+  assert(typ == "std::vector<Point>")
+end
+
+do
+  local size, typ = c.desc_to_ctype(d.CircularList(Node, "next", 0x1000), 4)
+  assert(size == 4)
+  assert(typ == "Node*")
+end
+
+do
+  local size, typ = c.desc_to_ctype(d.CircularList(Node, "next", 0x1000), 8)
+  assert(size == 8)
+  assert(typ == "Node*")
+end
+
+do
+  c.struct_to_cdecl(Point)
+end
