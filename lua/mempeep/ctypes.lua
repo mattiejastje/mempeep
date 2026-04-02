e5743dac61700f499f818b0bddc5f397e9871fc9
mattiejas <mattiejastje@gmail.com>
2026-04-02 17:59:12 +0100

    Rename, split between remote and native generation.

---
+++ lua/mempeep/ctypes.lua
@@ -0,0 +1,145 @@
+--- Dump C style version of descriptor or struct.
+
+local M = {}
+
+local remote_ctype_impl = {}
+
+function M.remote_ctype(desc, addr_size)
+  local impl = remote_ctype_impl[desc.tag]
+  assert(impl, "unknown descriptor tag: " .. tostring(desc.tag))
+  return impl(desc, addr_size)
+end
+
+local native_ctype_impl = {}
+
+function M.native_ctype(desc)
+  local impl = native_ctype_impl[desc.tag]
+  assert(impl, "unknown descriptor tag: " .. tostring(desc.tag))
+  return impl(desc)
+end
+
+local fmt_to_ctype_impl = {}
+fmt_to_ctype_impl.i1 = "int8_t"
+fmt_to_ctype_impl.i2 = "int16_t"
+fmt_to_ctype_impl.i4 = "int32_t"
+fmt_to_ctype_impl.i8 = "int64_t"
+fmt_to_ctype_impl.I1 = "uint8_t"
+fmt_to_ctype_impl.I2 = "uint16_t"
+fmt_to_ctype_impl.I4 = "uint32_t"
+fmt_to_ctype_impl.I8 = "uint64_t"
+fmt_to_ctype_impl.f = "float"
+fmt_to_ctype_impl.d = "double"
+
+local fmt_to_ctype = function(fmt)
+  typ = fmt_to_ctype_impl[fmt]
+  assert(typ, "unknown format '" .. tostring(fmt) .. "'")
+  return typ
+end
+
+remote_ctype_impl.Primitive = function(desc, addr_size)
+  assert(desc.fmt)
+  return string.packsize(desc.fmt), fmt_to_ctype(desc.fmt)
+end
+
+native_ctype_impl.Primitive = function(desc)
+  assert(desc.fmt)
+  return fmt_to_ctype(desc.fmt)
+end
+
+remote_ctype_impl.RawAddr = function(desc, addr_size)
+  return addr_size, "void*"
+end
+
+native_ctype_impl.RawAddr = function(desc)
+  return "uintptr_t"
+end
+
+remote_ctype_impl.Ref = function(desc, addr_size)
+  _, ref_ctype = M.remote_ctype(desc.desc, addr_size)
+  return addr_size, ref_ctype .. "*"
+end
+
+native_ctype_impl.Ref = function(desc)
+  return M.native_ctype(desc.desc)
+end
+
+remote_ctype_impl.NullableRef = remote_ctype_impl.Ref
+
+native_ctype_impl.NullableRef = function(desc)
+  return "std::optional<" .. M.native_ctype(desc.desc) .. ">"
+end
+
+remote_ctype_impl.Array = function(desc, addr_size)
+  local ref_size, ref_ctype = M.remote_ctype(desc.desc, addr_size)
+  return desc.n * ref_size, "std::array<" .. ref_ctype .. ", " .. desc.n .. ">"
+end
+
+native_ctype_impl.Array = function(desc)
+  return "std::array<" .. M.native_ctype(desc.desc) .. ", " .. desc.n .. ">"
+end
+
+remote_ctype_impl.Vector = function(desc, addr_size)
+  local _, ref_ctype = M.remote_ctype(desc.desc, addr_size)
+  return 2 * addr_size, "std::vector<" .. ref_ctype .. ">"
+end
+
+remote_ctype_impl.CircularList = function(desc, addr_size)
+  _, ref_ctype = M.remote_ctype(desc.desc, addr_size)
+  return addr_size, ref_ctype .. "*"
+end
+
+native_ctype_impl.CircularList = function(desc)
+  return "std::vector<" .. M.native_ctype(desc.desc) .. ">"
+end
+
+remote_ctype_impl.Struct = function(desc, addr_size)
+  local size = 0
+  for _, item in ipairs(desc.fields) do
+    if item.tag == "Skip" then
+      size = size + item.n
+    elseif item.tag == "Seek" then
+      size = item.n
+    elseif item.tag == "Field" then
+      local field_size, _ = M.remote_ctype(item.desc, addr_size)
+      size = size + field_size
+    end
+  end
+  return size, desc.name
+end
+
+native_ctype_impl.Struct = function(desc)
+  return desc.name
+end
+
+function M.remote_struct_cdecl(desc, addr_size)
+  assert(desc.tag == "Struct", "descriptor must be Struct, but got " .. tostring(desc.tag))
+  print(string.format("struct %s {", desc.name))
+  local offset = 0
+  for _, item in ipairs(desc.fields) do
+    if item.tag == "Skip" then
+      offset = offset + item.n
+    elseif item.tag == "Seek" then
+      offset = item.n
+    elseif item.tag == "Field" then
+      local field_size, field_ctype = M.remote_ctype(item.desc, addr_size)
+      print(string.format("  %s %s;  // offset 0x%x", field_ctype, item.key, offset))
+      offset = offset + field_size
+    end
+  end
+  print("};")
+end
+
+function M.native_struct_cdecl(desc, addr_size)
+  assert(desc.tag == "Struct", "descriptor must be Struct, but got " .. tostring(desc.tag))
+  print(string.format("struct %s {", desc.name))
+  local offset = 0
+  for _, item in ipairs(desc.fields) do
+    if item.tag == "Field" then
+      local field_ctype = M.native_ctype(item.desc, addr_size)
+      print(string.format("  %s %s;", field_ctype, item.key))
+    end
+  end
+  print("};")
+end
+
+return M
--- lua/test/test_ctypes.lua
+++ lua/test/test_ctypes.lua
@@ -1,5 +1,5 @@
 local d = require("mempeep.descriptors")
-local c = require("mempeep.ctype")
+local c = require("mempeep.ctypes")
 
 local Int8 = d.Primitive("i1")
 local Int16 = d.Primitive("i2")
@@ -9,89 +9,90 @@ local Point = d.Struct("Point", { d.Field(Int16, "x"), d.Seek(8), d.Field(Int16,
 local Node = d.Struct("Node", { d.Field(Int64, "data"), d.Field(d.RawAddr(), "next") })
 
 do
-  local size, typ = c.desc_to_ctype(Int8, 4)
+  local size, typ = c.remote_ctype(Int8, 4)
   assert(size == 1)
   assert(typ == "int8_t")
 end
 
 do
-  local size, typ = c.desc_to_ctype(Int64, 4)
+  local size, typ = c.remote_ctype(Int64, 4)
   assert(size == 8)
   assert(typ == "int64_t")
 end
 
 do
-  local size, typ = c.desc_to_ctype(Point, 4)
+  local size, typ = c.remote_ctype(Point, 4)
   assert(size == 12)
   assert(typ == "Point")
 end
 
 do
-  local size, typ = c.desc_to_ctype(d.RawAddr(), 4)
+  local size, typ = c.remote_ctype(d.RawAddr(), 4)
   assert(size == 4)
   assert(typ == "void*")
 end
 
 do
-  local size, typ = c.desc_to_ctype(d.RawAddr(), 8)
+  local size, typ = c.remote_ctype(d.RawAddr(), 8)
   assert(size == 8)
   assert(typ == "void*")
 end
 
 do
-  local size, typ = c.desc_to_ctype(d.Ref(Point), 4)
+  local size, typ = c.remote_ctype(d.Ref(Point), 4)
   assert(size == 4)
   assert(typ == "Point*")
 end
 
 do
-  local size, typ = c.desc_to_ctype(d.Ref(Point), 8)
+  local size, typ = c.remote_ctype(d.Ref(Point), 8)
   assert(size == 8)
   assert(typ == "Point*")
 end
 
 do
-  local size, typ = c.desc_to_ctype(d.NullableRef(Point), 4)
+  local size, typ = c.remote_ctype(d.NullableRef(Point), 4)
   assert(size == 4)
   assert(typ == "Point*")
 end
 
 do
-  local size, typ = c.desc_to_ctype(d.NullableRef(Point), 8)
+  local size, typ = c.remote_ctype(d.NullableRef(Point), 8)
   assert(size == 8)
   assert(typ == "Point*")
 end
 
 do
-  local size, typ = c.desc_to_ctype(d.Array(Point, 10), 4)
+  local size, typ = c.remote_ctype(d.Array(Point, 10), 4)
   assert(size == 120)
   assert(typ == "std::array<Point, 10>")
 end
 
 do
-  local size, typ = c.desc_to_ctype(d.Vector(Point, 0x1000), 4)
+  local size, typ = c.remote_ctype(d.Vector(Point, 0x1000), 4)
   assert(size == 8)
   assert(typ == "std::vector<Point>")
 end
 
 do
-  local size, typ = c.desc_to_ctype(d.Vector(Point, 0x1000), 8)
+  local size, typ = c.remote_ctype(d.Vector(Point, 0x1000), 8)
   assert(size == 16)
   assert(typ == "std::vector<Point>")
 end
 
 do
-  local size, typ = c.desc_to_ctype(d.CircularList(Node, "next", 0x1000), 4)
+  local size, typ = c.remote_ctype(d.CircularList(Node, "next", 0x1000), 4)
   assert(size == 4)
   assert(typ == "Node*")
 end
 
 do
-  local size, typ = c.desc_to_ctype(d.CircularList(Node, "next", 0x1000), 8)
+  local size, typ = c.remote_ctype(d.CircularList(Node, "next", 0x1000), 8)
   assert(size == 8)
   assert(typ == "Node*")
 end
 
 do
-  c.struct_to_cdecl(Point)
+  c.remote_struct_cdecl(Point)
+  c.native_struct_cdecl(Point)
 end
